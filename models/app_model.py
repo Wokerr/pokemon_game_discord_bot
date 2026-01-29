6edf1bc18feeb7202064651288a94142e6ebd559
Emmanuel Arenas <emmanuelare00@gmail.com>
2026-01-29 07:07:57 -0500

    Refactor: app_model, Guild mode add, Pokemon and User model update

--- models/app_model.py
+++ models/app_model.py
@@ -4,21 +4,17 @@ from datetime import datetime
 ### USER MODEL ###
 
 class User(BaseModel):
-    guild_id: int # This must be a server ID in discord
     user_id: int # This must be a discord ID
+
+### GUILD MODEL ###
+
+class Guild(BaseModel):
+    guild_id: int
 
 ### POKEMON MODEL ###
 
 class Pokemon(BaseModel):
     pokemon_name: str
+    pokemon_id: int
 
-class Type(BaseModel):
-    type: str
-
-### POKEDEX MODEL ###
-
-class Pokedex(BaseModel): # Here's where the pokemon will saved with discord ID of his owner
-    user_id: int # Relationship between User and Pokedex
-    pokemon_id: int # Relationship between Pokemon and Pokedex
-    capture_date: datetime # Moment where the pokemon was picked up
 
